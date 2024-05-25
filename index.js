47629389f47fe4cdd858505305599f4458f86c76
MirzaMuhammadBaig <mirzamuhammadbaig328@gmail.com>
2024-05-25 13:08:47 +0500

    completed

---
+++ index.js
@@ -0,0 +1,50 @@
+import readline from 'readline';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+let secretNumber;
+let attempts = 0;
+function startNewGame() {
+    secretNumber = generateRandomNumber();
+    console.log(secretNumber);
+    attempts = 0;
+    console.log("Welcome to the Number Guessing Game!");
+    console.log("I've selected a random number between 1 and 100. Try to guess it.");
+    askForGuess();
+}
+function askForGuess() {
+    rl.question("Enter your guess: ", (input) => {
+        const guess = parseInt(input);
+        if (isNaN(guess)) {
+            console.log("Please enter a valid number.");
+            askForGuess();
+        }
+        else {
+            attempts++;
+            if (guess === secretNumber) {
+                console.log(`Congratulations! You guessed the number ${secretNumber} in ${attempts} attempts.`);
+                rl.question("Do you want to play again? (yes/no): ", (playAgain) => {
+                    if (playAgain.toLowerCase() === 'yes') {
+                        startNewGame();
+                    }
+                    else {
+                        rl.close();
+                    }
+                });
+            }
+            else if (guess < secretNumber) {
+                console.log("Try a higher number.");
+                askForGuess();
+            }
+            else {
+                console.log("Try a lower number.");
+                askForGuess();
+            }
+        }
+    });
+}
+startNewGame();
